124d2647fafa28682ce8e47f5b453f715ebd0d67
Browan27 <Browan27@yahoo.com>
2016-10-07 11:40:46 -0500

    added tests and functionality for database.

--- src/main/java/edu/jsu/mcis/gamegogy/Gamegogy.java
+++ src/main/java/edu/jsu/mcis/gamegogy/Gamegogy.java
@@ -6,20 +6,7 @@ import java.util.*;
 public class Gamegogy {
     
     public static String findID(String type, String id) {
-        List<String[]> list = CSV.fileReader(file);
-        String[] array;
-        
-        for(int i =0; i < list.size(); i++) {
-            array = list.get(i);
-            if(id.equals(array[0])) {
-                String string = "[" + array[0] + "]";
-                for(int j = 1; j < array.length; j++) {
-                    string += " " + array[j];
-                }
-                return string;
-            }
-        }
-        return null;
+        return "";
     }
     
     
