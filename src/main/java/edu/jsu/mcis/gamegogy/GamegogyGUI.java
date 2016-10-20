f01253e2d10d161fcfcdb0cf8425916385774e72
Browan27 <Browan27@yahoo.com>
2016-10-20 10:11:07 -0500

    adds courseGrades

--- src/main/java/edu/jsu/mcis/gamegogy/GamegogyGUI.java
+++ src/main/java/edu/jsu/mcis/gamegogy/GamegogyGUI.java
@@ -5,6 +5,46 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class GamegogyGUI {
+public class GamegogyGUI extends JFrame{
+    private Gamegogy gamegogy;
+    private Course[] courseObjs;
+    private String[] courseAssignments = {" "};
+    /*
+        TODO: remove assignment of empty string set
+    */
 
+    public GamegogyGUI() {
+        gamegogy = new Gamegogy();
+        Dimension dim = getPreferredSize();
+        
+        courseObjs = (Course[])gamegogy.findAll("course");
+        
+        /*
+            TODO: add function to get course assignments
+        */
+        //CourseGrades[] courseAssigments = gamegogy.;
+        
+        SelectionPanel selectionPanel = 
+                new SelectionPanel(courseObjs, courseAssignments);
+        LeaderboardPanel leaderboardPanel = 
+                new LeaderboardPanel();
+        InformationPanel infoPanel = 
+                new InformationPanel();
+        
+        this.add(selectionPanel);
+        //mainFrame.add(leaderboardPanel);
+        //mainFrame.add(infoPanel);
+    }
+
+    @Override
+    public final Dimension getPreferredSize() {
+        return new Dimension(400, 400);
+    }
+
+    public static void main(String[] args) {
+        GamegogyGUI frame = new GamegogyGUI();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.pack();
+    }
 }
