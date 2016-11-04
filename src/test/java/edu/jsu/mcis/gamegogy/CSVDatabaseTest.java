cebf25b0363fc56ba3c2f4d9ab57cb683557aa37
Browan27 <Browan27@yahoo.com>
2016-11-04 12:17:10 -0500

    adds robot file for feature 5 and makes a database test for csv resources and json resources.

--- src/test/java/edu/jsu/mcis/gamegogy/CSVDatabaseTest.java
+++ src/test/java/edu/jsu/mcis/gamegogy/CSVDatabaseTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 
-public class DatabaseTest {
+public class CSVDatabaseTest {
     private Database database;
     
     @Before
---
+++ src/test/java/edu/jsu/mcis/gamegogy/JSONDatabaseTest.java
@@ -0,0 +1,117 @@
+package edu.jsu.mcis.gamegogy;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class JSONDatabaseTest {
+    private Database database;
+    
+    @Before
+    public void setUp() {
+        database = new Database(new JSONResource());
+    }
+    
+    @Test
+    public void testGetStudent() {
+        String[] info = {"111111","Jerrod","Shields","jshields"};
+        Student testStudent = new Student(info);
+        
+        Student dataStudent = database.getStudent("111111");
+        
+        assertEquals(testStudent.getID(), dataStudent.getID());
+        assertEquals(testStudent.getFirstName(), dataStudent.getFirstName());
+        assertEquals(testStudent.getLastName(), dataStudent.getLastName());
+        assertEquals(testStudent.getEmail(), dataStudent.getEmail());
+    }
+    
+    @Test
+    public void testGetCourse() {
+        String[] info = {"99000","Spring","2013","11"};
+		Course testCourse = new Course(info);
+        
+        Course dataCourse = database.getCourse("99000");
+        
+        assertEquals(testCourse.getID(), dataCourse.getID());
+        assertEquals(testCourse.getSemester(), dataCourse.getSemester());
+        assertEquals(testCourse.getYear(), dataCourse.getYear());
+        assertEquals(testCourse.getEnrolled(), dataCourse.getEnrolled());
+    }
+    
+    @Test
+    public void testGetStudentFromMiddle() {
+        String[] info = {"111232","Isidro","Thomas","ithomas"};
+        Student testStudent = new Student(info);
+        
+        Student dataStudent = database.getStudent("111232");
+        
+        assertEquals(testStudent.getID(), dataStudent.getID());
+        assertEquals(testStudent.getFirstName(), dataStudent.getFirstName());
+        assertEquals(testStudent.getLastName(), dataStudent.getLastName());
+        assertEquals(testStudent.getEmail(), dataStudent.getEmail());
+    }
+    
+    @Test
+    public void testGetCourseFromMiddle() {
+        String[] info = {"99019","Fall","2013","20"};
+		Course testCourse = new Course(info);
+        
+        Course dataCourse = database.getCourse("99019");
+        
+        assertEquals(testCourse.getID(), dataCourse.getID());
+        assertEquals(testCourse.getSemester(), dataCourse.getSemester());
+        assertEquals(testCourse.getYear(), dataCourse.getYear());
+        assertEquals(testCourse.getEnrolled(), dataCourse.getEnrolled());
+    }
+    
+    @Test
+    public void testGetStudentFromEnd() {
+        String[] info = {"111410","Rory","Dominguez","rdominguez"};
+        Student dataStudent = new Student(info);
+        
+        Student testStudent = database.getStudent("111410");
+        
+        assertEquals(testStudent.getID(), dataStudent.getID());
+        assertEquals(testStudent.getFirstName(), dataStudent.getFirstName());
+        assertEquals(testStudent.getLastName(), dataStudent.getLastName());
+        assertEquals(testStudent.getEmail(), dataStudent.getEmail());
+    }
+    
+    @Test
+    public void testGetStudentFromReturnedArray() {
+        Student[] array = database.getAllStudents();
+        Student dataStudent = array[0];
+        
+        Student testStudent = database.getStudent("111111");
+        
+        assertEquals(testStudent.getID(), dataStudent.getID());
+        assertEquals(testStudent.getFirstName(), dataStudent.getFirstName());
+        assertEquals(testStudent.getLastName(), dataStudent.getLastName());
+        assertEquals(testStudent.getEmail(), dataStudent.getEmail());
+    }
+    
+    @Test
+    public void testGetCourseFromReturnedArray() {
+        Course[] array = database.getAllCourses();
+		Course dataCourse = array[0];
+        
+        Course testCourse = database.getCourse("99000");
+        
+        assertEquals(testCourse.getID(), dataCourse.getID());
+        assertEquals(testCourse.getSemester(), dataCourse.getSemester());
+        assertEquals(testCourse.getYear(), dataCourse.getYear());
+        assertEquals(testCourse.getEnrolled(), dataCourse.getEnrolled());
+    }
+    
+    @Test
+    public void testGetRandomAssignmentFromFront() {
+        CourseGrades courseGrades = database.getGrades("99000");
+        assertEquals("43.0", courseGrades.getGrades("111262", "Assignment 9"));
+    }
+    
+    @Test
+    public void testGetTotalFromMiddle() {
+        CourseGrades courseGrades = database.getGrades("99017");
+        assertEquals("845.0", courseGrades.getGrades("111224", "Total"));
+    }
+}
